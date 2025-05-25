75b552ccf3061ec21784f5ca958094ed9de3385f
geraldmas <geraldmasjesse@gmail.com>
2025-05-25 15:32:58 +0200

    bug

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import path from 'path';
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  mode: 'production',
+  server: {
+    fs: {
+      strict: true
+    },
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  },
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
